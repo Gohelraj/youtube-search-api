a2a0e5ed50160c9bd81577d2205f0346ac993ef4
Raj Gohel <gohelraj11111996@gmail.com>
2022-07-31 02:03:58 +0530

    add missing DB connection closing statement

--- api/repository/youtube_repository.go
+++ api/repository/youtube_repository.go
@@ -5,7 +5,6 @@ import (
 	"github.com/Gohelraj/youtube-search-api/api/model"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
-	"log"
 	"time"
 )
 
@@ -29,36 +28,25 @@ func NewYoutubeRepo(pgxPool *pgxpool.Pool) YoutubeRepository {
 }
 
 func (youtubeRepo youtubeRepository) InsertVideos(videos []model.VideoMetadata) error {
-	conn, err := youtubeRepo.pgxPool.Acquire(context.Background())
-	if err != nil {
-		log.Printf("Couldn't get a connection with the database. Reason %v", err)
-		return err
-	}
-	defer conn.Release()
 	batch := &pgx.Batch{}
 	for _, video := range videos {
 		currentTime := time.Now().UTC()
 		// here we are using "ON CONFLICT DO NOTHING" to avoid duplicate entries/duplicate primary key violation errors
 		batch.Queue("INSERT INTO videos (youtube_id, title, description, published_at, created_at, updated_at, thumbnail_url) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT DO NOTHING", video.YoutubeID, video.Title, video.Description, video.PublishedAt, currentTime, currentTime, video.ThumbnailURL)
 	}
-	result := conn.SendBatch(context.Background(), batch)
+	result := youtubeRepo.pgxPool.SendBatch(context.Background(), batch)
 	for i := 0; i < batch.Len(); i++ {
 		_, err := result.Exec()
 		if err != nil {
 			return err
 		}
 	}
+	defer result.Close()
 	return nil
 }
 
 func (youtubeRepo youtubeRepository) GetAvailableLastPageToken() (pageToken string, err error) {
-	conn, err := youtubeRepo.pgxPool.Acquire(context.Background())
-	if err != nil {
-		log.Printf("Couldn't get a connection with the database. Reason %v", err)
-		return "", err
-	}
-	defer conn.Release()
-	row := conn.QueryRow(context.Background(), "SELECT next_page_token FROM page_tokens WHERE is_used = false ORDER BY created_at DESC LIMIT 1")
+	row := youtubeRepo.pgxPool.QueryRow(context.Background(), "SELECT next_page_token FROM page_tokens WHERE is_used = false ORDER BY created_at DESC LIMIT 1")
 	err = row.Scan(&pageToken)
 	if err != nil && err != pgx.ErrNoRows {
 		return "", err
@@ -67,36 +55,18 @@ func (youtubeRepo youtubeRepository) GetAvailableLastPageToken() (pageToken stri
 }
 
 func (youtubeRepo youtubeRepository) InsertNextPageToken(pageToken string) error {
-	conn, err := youtubeRepo.pgxPool.Acquire(context.Background())
-	if err != nil {
-		log.Printf("Couldn't get a connection with the database. Reason %v", err)
-		return err
-	}
-	defer conn.Release()
-	_, err = conn.Exec(context.Background(), "INSERT INTO page_tokens (next_page_token, created_at) VALUES ($1, $2) ON CONFLICT DO NOTHING", pageToken, time.Now().UTC())
+	_, err := youtubeRepo.pgxPool.Exec(context.Background(), "INSERT INTO page_tokens (next_page_token, created_at) VALUES ($1, $2) ON CONFLICT DO NOTHING", pageToken, time.Now().UTC())
 	return err
 }
 
 func (youtubeRepo youtubeRepository) MarkPageTokenAsUsed(pageToken string) error {
-	conn, err := youtubeRepo.pgxPool.Acquire(context.Background())
-	if err != nil {
-		log.Printf("Couldn't get a connection with the database. Reason %v", err)
-		return err
-	}
-	defer conn.Release()
-	_, err = conn.Exec(context.Background(), "UPDATE page_tokens SET is_used = true WHERE next_page_token = $1", pageToken)
+	_, err := youtubeRepo.pgxPool.Exec(context.Background(), "UPDATE page_tokens SET is_used = true WHERE next_page_token = $1", pageToken)
 	return err
 }
 
 func (youtubeRepo youtubeRepository) GetVideos(limit int, offset int) ([]model.VideoMetadata, error) {
-	conn, err := youtubeRepo.pgxPool.Acquire(context.Background())
-	if err != nil {
-		log.Printf("Couldn't get a connection with the database. Reason %v", err)
-		return []model.VideoMetadata{}, err
-	}
-	defer conn.Release()
 	videos := []model.VideoMetadata{}
-	rows, err := conn.Query(context.Background(), "SELECT id, youtube_id, title, description, published_at, thumbnail_url FROM videos ORDER BY published_at DESC LIMIT $1 OFFSET $2", limit, offset)
+	rows, err := youtubeRepo.pgxPool.Query(context.Background(), "SELECT id, youtube_id, title, description, published_at, thumbnail_url FROM videos ORDER BY published_at DESC LIMIT $1 OFFSET $2", limit, offset)
 	if err != nil {
 		return nil, err
 	}
@@ -113,14 +83,8 @@ func (youtubeRepo youtubeRepository) GetVideos(limit int, offset int) ([]model.V
 }
 
 func (youtubeRepo youtubeRepository) SearchVideos(searchString string) ([]model.VideoMetadata, error) {
-	conn, err := youtubeRepo.pgxPool.Acquire(context.Background())
-	if err != nil {
-		log.Printf("Couldn't get a connection with the database. Reason %v", err)
-		return []model.VideoMetadata{}, err
-	}
-	defer conn.Release()
 	videos := []model.VideoMetadata{}
-	rows, err := conn.Query(context.Background(), "SELECT id, youtube_id, title, description, published_at, thumbnail_url FROM videos WHERE document_with_weights @@ plainto_tsquery($1) ORDER BY ts_rank(document_with_weights, plainto_tsquery($1)) DESC, published_at DESC", searchString)
+	rows, err := youtubeRepo.pgxPool.Query(context.Background(), "SELECT id, youtube_id, title, description, published_at, thumbnail_url FROM videos WHERE document_with_weights @@ plainto_tsquery($1) ORDER BY ts_rank(document_with_weights, plainto_tsquery($1)) DESC, published_at DESC", searchString)
 	if err != nil {
 		return nil, err
 	}
